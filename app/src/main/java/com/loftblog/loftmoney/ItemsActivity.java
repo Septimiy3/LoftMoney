55e575b587ba3e12c0df5d9e5e76f409964ab026
Dimas <droxdtv@mail.ru>
2019-02-06 22:01:38 +0800

    add background and Decorator

--- app/src/main/java/com/loftblog/loftmoney/ItemsActivity.java
+++ app/src/main/java/com/loftblog/loftmoney/ItemsActivity.java
@@ -1,14 +1,16 @@
 package com.loftblog.loftmoney;
 
-
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import static android.widget.GridLayout.VERTICAL;
 
 public class ItemsActivity extends AppCompatActivity {
 
@@ -21,29 +23,28 @@ public class ItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_items);
 
 
-
         adapter = new ItemsAdapter(this);
 
         recycler = findViewById(R.id.recycler);
         recycler.setAdapter(adapter);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setLayoutManager(new LinearLayoutManager(this ));
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        recycler.addItemDecoration(decoration);
 
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Молоко","70р"));
-        items.add(new Item("Масло","70р"));
-        items.add(new Item("Рис","70р"));
-        items.add(new Item("Хдуб","70р"));
-        items.add(new Item("фывфыв","70р"));
-        items.add(new Item("ыфвф","70р"));
-        items.add(new Item("123","70р"));
-        items.add(new Item("выфв","70р"));
-        items.add(new Item("ывф","70р"));
-        items.add(new Item("123аыфв","70р"));
-        items.add(new Item("рпара","70р"));
-        items.add(new Item("41кмафы","70р"));
+        items.add(new Item("Молоко","50p"));
+        items.add(new Item("Жираф","5000000p"));
+        items.add(new Item("Кукла","31"));
+        items.add(new Item("Вино","200"));
+        items.add(new Item("Сосиска","5p"));
+        items.add(new Item("Сосиска","5p"));
+        items.add(new Item("Йогурт","5p"));
+        items.add(new Item("Сосиска","5p"));
+        items.add(new Item("Сосиска","5p"));
+        items.add(new Item("Табак","5p"));
+        items.add(new Item("Сосиска","5p"));
 
         adapter.setItems(items);
+
     }
-
-
 }
